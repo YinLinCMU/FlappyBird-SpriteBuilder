4051032e41b44228cb2923b3873fe86f8bb87f76
Yin Lin <linyin1112@gmail.com>
2015-01-17 18:24:30 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,20 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta; //delta = 1/60 of a second
+    //check to see if 2s have passed
+    if(timeSinceObstacle > 2.0f){
+    //add new obstacle
+        [slef addObstacle];
+        //then reset the timmer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
